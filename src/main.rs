dd655b2017ad38d881fd7338d5ee3474ca457b19
Andrea Talenti <23279528+RenzoTale88@users.noreply.github.com>
2024-10-09 16:39:16 +0100

    Progress in parsing data

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use clap::{Command, Arg};
 fn process_csv(file_path: &str, prefix: &str) -> Result<(), Box<dyn std::error::Error>>{
     // Switch to start converting to PED
     let mut start_parsing: bool = false;
+    let mut a1_index: u8;
+    let mut a2_index: u8;
 
     // Input readers, accepting either a file or stdin
     let reader: Box<dyn Read> = if file_path == "-" {
@@ -19,16 +21,25 @@ fn process_csv(file_path: &str, prefix: &str) -> Result<(), Box<dyn std::error::
     for line in reader.lines(){
         let line = line?;
         if !line.is_empty(){
-            let split_line = line.split(' ');
+            if line.contains("[input]") {
+                continue;
+            } else if line.contains("[data]") {
+                start_parsing = true;
+            } else if !start_parsing {
+                let split_line = line.split('=');
+                let split_line: Vec<&str> = split_line.collect();
+                let key = split_line[0];
+                let val: i64 = split_line[1].parse().unwrap();
+                println!("{}: {}", key, val);
+            }
         }
     }
-
     Ok(())
 }
 
 
 fn main() {
-    let matches = Command::new("fastix")
+    let matches = Command::new("ilmn2ped")
         .version("0.1.0")
         .author("Andrea Talenti <andrea.talenti@ed.ac.uk>")
         .about("Easy conversion from Illumina manifest CSV file to ped/map")
@@ -41,10 +52,12 @@ fn main() {
         )
         .arg(
             Arg::new("OUTPUT")
+                .required(false)
                 .short('o')
                 .long("output")
                 .num_args(1)
-                .help("Output file prefix"),
+                .help("Output file prefix")
+                .default_value("None"),
         )
         .get_matches();
 
@@ -54,5 +67,4 @@ fn main() {
 
     process_csv(filename, prefix);
 
-    println!("Hello, world!");
 }
