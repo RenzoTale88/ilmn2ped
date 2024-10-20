c0f4faf1b820d626373bef0f6f6ab03d70e80230
Andrea Talenti <23279528+RenzoTale88@users.noreply.github.com>
2024-10-20 11:07:07 +0100

    Restructuring

--- src/report.rs
+++ src/report.rs
@@ -69,7 +69,8 @@ pub fn process_csv(
 ) -> Result<(), Box<dyn std::error::Error>> {
     // Start parsing the data
     let mut start_parsing: bool = false;
-    let mut parsed_header: bool = false;
+    let mut end_parsing: bool = false;
+    let mut parse_header: bool = false;
     // Sample names and number of expected alleles
     // Sample name can be null at the beginning, so that we can
     // initialize it at the first line of the table.
@@ -86,9 +87,6 @@ pub fn process_csv(
     let mut sample_col_index: usize = 0;
     let mut snp_col_index: usize = 1;
     let mut site_idx: usize = 0;
-    // Create output files
-    let mut pedfile = File::create(format!("{out_root}.ped"))?;
-    let mut mapfile = File::create(format!("{out_root}.map"))?;
 
     // Define input file delimiter based on the suffix
     let delimiter = if file_path.ends_with(".csv") {
@@ -103,104 +101,119 @@ pub fn process_csv(
     } else {
         Box::new(BufReader::new(File::open(file_path)?))
     };
-    let reader = BufReader::new(reader);
+    let mut reader = BufReader::new(reader);
+    let mut line: String = String::new();
 
-    // Process the input file
-    for line_result in reader.lines() {
-        // Unpack the line
-        let line = line_result?;
+    // Parse the header first
+    while !start_parsing {
+        reader.read_line(&mut line)?;
         if !line.is_empty() {
-            let split_line: Vec<&str> = line.split(delimiter).collect();
+            let split_line: Vec<&str> = line.trim().split(delimiter).collect();
+            if parse_header {
+                // Parse the header for the right indexes
+                let validated = validate_header(split_line.clone(), coding);
+                match validated {
+                    Ok(snp_col_name) => {
+                        // First, we validate the header
+                        let a1_name = get_valid_column_id(coding, "1").unwrap();
+                        let a2_name = get_valid_column_id(coding, "2").unwrap();
+                        // Get allele input columns
+                        a1_index = split_line
+                        .iter()
+                        .position(|&value| value == a1_name)
+                        .unwrap();
+                        a2_index = split_line
+                        .iter()
+                        .position(|&value| value == a2_name)
+                        .unwrap();
+                
+                        // Sample column index
+                        sample_col_index = split_line
+                        .iter()
+                        .position(|&value| value.to_lowercase().contains(&"sample"))
+                        .unwrap();
 
-            if line.contains("[Header]") {
-                continue;
-            } else if line.contains("[Data]") {
+                        // SNP column index
+                        snp_col_index = split_line
+                        .iter()
+                        .position(|&value| value.contains(&snp_col_name))
+                        .unwrap();
+                    },
+                    Err(e) => panic!("{e}"),
+                };
+                println!("Columns for coding {} found.", coding);
                 start_parsing = true;
-            } else if !start_parsing {
-                let key = split_line[0];
-                if key == "Num SNPs" {
-                    let num_sites: i64 = split_line[1].parse()?;
-                    num_alleles = num_sites * 2;
-                    genotypes = Vec::with_capacity(num_alleles as usize);
-                    println!("Found {} sites", num_sites);
-                }
-            } else {
-                if !parsed_header {
-                    let validated = validate_header(split_line.clone(), coding);
-                    match validated {
-                        Ok(snp_col_name) => {
-                            // First, we validate the header
-                            let a1_name = get_valid_column_id(coding, "1").unwrap();
-                            let a2_name = get_valid_column_id(coding, "2").unwrap();
-
-                            // Get allele input columns
-                            a1_index = split_line
-                                .iter()
-                                .position(|&value| value == a1_name)
-                                .unwrap();
-                            a2_index = split_line
-                                .iter()
-                                .position(|&value| value == a2_name)
-                                .unwrap();
-
-                            // Sample column index
-                            sample_col_index = split_line
-                                .iter()
-                                .position(|&value| value.to_lowercase().contains(&"sample"))
-                                .unwrap();
-                            // SNP column index
-                            snp_col_index = split_line
-                                .iter()
-                                .position(|&value| value.to_lowercase().contains(&snp_col_name))
-                                .unwrap();
-                            if !map.is_none() {
-                                site_metadata = Some(load_map(map.unwrap(), snp_col_name)?);
-                            };
-                        }
-                        Err(e) => panic!("{e}"),
-                    };
-                    println!("Columns for coding {} found.", coding);
-                    parsed_header = true;
-                } else {
-                    let local_sample = split_line[sample_col_index].to_string();
-                    let local_var = split_line[snp_col_index].to_string();
-                    if !variants.contains(&local_var) {
-                        variants.push(local_var)
-                    }
-                    if sample_name.is_none() {
-                        sample_name = Some(local_sample.clone());
-                        genotypes = Vec::with_capacity(num_alleles as usize);
-                    } else if sample_name.as_ref().unwrap() != &local_sample {
-                        site_idx = 0;
-                        let sample = sample_name.take().unwrap();
-                        writeln!(
-                            pedfile,
-                            "{sample} {sample} 0 0 0 -9 {}",
-                            genotypes.join(" ")
-                        )?;
-                        sample_name = Some(local_sample.clone());
-                        genotypes = Vec::with_capacity(num_alleles as usize);
-                    }
-
-                    let a1: String = split_line[a1_index].to_string().replace("-", "0");
-                    let a2: String = split_line[a2_index].to_string().replace("-", "0");
-                    let a1_pos: usize = ((site_idx) * 2) as usize;
-                    let a2_pos: usize = a1_pos + 1;
-
-                    if a1_pos < genotypes.len() {
-                        genotypes[a1_pos] = a1;
-                    } else {
-                        genotypes.insert(a1_pos, a1);
-                    }
-
-                    if a2_pos < genotypes.len() {
-                        genotypes[a2_pos] = a2;
-                    } else {
-                        genotypes.insert(a2_pos, a2);
-                    }
-                    site_idx += 1;
-                }
+            // Get expected number of variants in the dataset
+            } else if split_line.contains(&"[Data]") {
+                parse_header = true;
+            } else if line.contains("Num SNPs"){
+                let num_sites: i64 = split_line[1].parse()?;
+                num_alleles = num_sites * 2;
+                genotypes = Vec::with_capacity(num_alleles as usize);
+                println!("Found {} sites", num_sites);
             }
+        }
+        line = String::new();
+    }
+    println!("Start parsing the data");
+    // Load map file, if provided
+    // if !map.is_none() {
+    //     site_metadata = Some(load_map(map.unwrap(), snp_col_name)?);
+    // };
+
+    // Print warning
+    // Create output files
+    let mut pedfile = File::create(format!("{out_root}.ped"))?;
+    let mut mapfile = File::create(format!("{out_root}.map"))?;
+
+    // Then process the sites
+    while !end_parsing {
+    // for line_result in reader.lines() {
+        // Unpack the line
+        reader.read_line(&mut line)?;
+        println!("{line}");
+        if !line.is_empty() {
+            let split_line: Vec<&str> = line.trim().split(delimiter).collect();
+            let local_sample = split_line[sample_col_index].to_string();
+            let local_var = split_line[snp_col_index].to_string();
+            if !variants.contains(&local_var) {
+                variants.push(local_var)
+            }
+            if sample_name.is_none() {
+                sample_name = Some(local_sample.clone());
+                genotypes = Vec::with_capacity(num_alleles as usize);
+            } else if sample_name.as_ref().unwrap() != &local_sample {
+                site_idx = 0;
+                let sample = sample_name.take().unwrap();
+                writeln!(
+                    pedfile,
+                    "{sample} {sample} 0 0 0 -9 {}",
+                    genotypes.join(" ")
+                )?;
+                sample_name = Some(local_sample.clone());
+                genotypes = Vec::with_capacity(num_alleles as usize);
+            }
+
+            let a1: String = split_line[a1_index].to_string().replace("-", "0");
+            let a2: String = split_line[a2_index].to_string().replace("-", "0");
+            let a1_pos: usize = ((site_idx) * 2) as usize;
+            let a2_pos: usize = a1_pos + 1;
+
+            if a1_pos < genotypes.len() {
+                genotypes[a1_pos] = a1;
+            } else {
+                genotypes.insert(a1_pos, a1);
+            }
+
+            if a2_pos < genotypes.len() {
+                genotypes[a2_pos] = a2;
+            } else {
+                genotypes.insert(a2_pos, a2);
+            }
+            site_idx += 1;
+            line = String::new();
+        } else {
+            end_parsing = true;
         }
     }
     // Save the last line to the output file
